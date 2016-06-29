82369252577dcd4e51814ebedae4b9f9f1ee29b7
kyuuki <kyuuki.japan@gmail.com>
2016-06-29 04:27:00 +0000

    add Want, Have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -5,13 +5,13 @@ class OwnershipsController < ApplicationController
     if params[:item_code]
       @item = Item.find_or_initialize_by(item_code: params[:item_code])
     else
-      @item = Item.find(params[:item_id])
+      @item = Item.find(params[:item_id])  # item_id でくるパターンがある？
     end
 
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      items = RakutenWebService::Ichiba::Item.search(itemCode: params[:item_code])
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -25,7 +25,11 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
+    if (params[:type] == "Want")
+      current_user.want(@item)
+    elsif (params[:type] == "Have")
+      current_user.have(@item)
+    end
 
   end
 
